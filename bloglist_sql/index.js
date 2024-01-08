062261fdeb03b3f73b53470d01eb43bb5fdd0cc7
danieljjyuen <dan@LAPTOP-AP8VLLC3.localdomain>
2024-01-08 11:55:25 -0500

    part 13 exercise 13.7

--- bloglist_sql/index.js
+++ bloglist_sql/index.js
@@ -1,14 +1,20 @@
 const express = require('express')
 const app = express()
+require('express-async-errors')
 
 const { PORT } = require('./util/config')
 const { connectToDatabase } = require('./util/db')
 
 const blogsRouter = require('./controllers/blogs')
+const { unknownEndpoint, errorHandler } = require('./util/middleware')
 
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const start = async () => {
     await connectToDatabase()
