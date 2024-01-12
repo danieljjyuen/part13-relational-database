639a7e221a5ef2a6c83b2fefa100c21e538bfc08
danieljjyuen <dan@LAPTOP-AP8VLLC3.localdomain>
2024-01-12 18:34:19 -0500

    part 13 exercise 13.22

--- bloglist_sql/controllers/readingLists.js
+++ bloglist_sql/controllers/readingLists.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const {ReadingList} = require('../models/index')
+const { tokenExtractor } = require('../util/middleware')
 
 router.post('/', async (req, res) => {
     try{
@@ -10,6 +11,24 @@ router.post('/', async (req, res) => {
         console.log(error)
         next()
     }
+})
+
+router.put('/:id', tokenExtractor, async (req, res) => {
+    try{
+        const readinglist = await ReadingList.findByPk(req.params.id)
+        console.log('readinglist: ' ,readinglist)
+        if(readinglist.userId === req.decodedToken.id){
+            readinglist.read = true
+            await readinglist.save()
+            return res.status(200).json(readinglist)
+        }else{
+           return res.status(400).json({error: 'unauthorized user'})
+        }
+    }catch(error) {
+        console.log(error)
+        next()
+    }
+
 })
 
 module.exports = router
