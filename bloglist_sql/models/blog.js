708bd03a2cbfc0455e3738737326f8df43c1e014
danieljjyuen <dan@LAPTOP-AP8VLLC3.localdomain>
2024-01-12 14:00:44 -0500

    part 13 exercise 13.19

--- bloglist_sql/models/blog.js
+++ bloglist_sql/models/blog.js
@@ -23,6 +23,14 @@ Blog.init({
     likes: {
         type:DataTypes.INTEGER,
         defaultValue: 0
+    },
+    year: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        validate: {
+            min: 1991,
+            max: new Date().getFullYear()
+        }
     }
 }, {
     sequelize,
